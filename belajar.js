86370dc61f914424c81c8217383317d175df107b
irfan <pajarakanirfan@gmail.com>
2025-09-10 14:52:45 +0700

    belaajar JS

---
+++ belajar.js
@@ -0,0 +1,113 @@
+// alert ("Thank you")
+// confirm ("kjdslksfdj")
+// prompt ("kjdshfhksdjf")
+// Date ("14 february 2006")
+
+// parseInt("ijnnstt")
+// setTimeout(irfan,5000)
+
+// function foo() {
+// alert("Our function just ran!");
+// } foo(); a
+
+// function alertArraySize(arr) {
+// alert(arr.length);
+// }
+
+// var test = [1,2,3,4,5];
+var no1 = 5;
+no1 = 5
+console.log(no1);
+
+no2 = 6 + 5; 
+console.log(no2);
+
+no3 = 9 - 2;
+console.log(no3);
+
+var number1 = 6 * 2;
+console.log(number1);
+
+
+var number2 = number1 / 2;
+console.log(number2);
+
+var mystaring = "Ahmad Irfan" ;
+console.log(mystaring)
+
+var myinteger = 12;
+console.log(myinteger)
+
+var mylogin = 9310141419482;
+console.log(mylogin);
+
+console.log()
+var myfloat = 5.5;
+console.log(myfloat);
+
+var mydouble = 9310141419482.22;
+console.log(mydouble);
+
+var mybolean = true ;
+console.log(mybolean)
+
+var myBoolean2 = false ;
+console.log(myBoolean2);
+
+var myNotANumber = NaN ;
+console.log(myNotANumber);
+
+var NaN_Example = 0 / 0 ;
+console.log(NaN_Example);
+
+var notDefined;
+console.log(notDefined)
+
+var myNull = null ;
+console.log(myNull)
+
+var myArray = [];
+console.log(myArray);
+
+var ClubFav = [ "Barcelona" , "Munyuk" , "demit" ];
+console.log(ClubFav);
+
+var mobilFav = [ "Toyota" , "Ferrari" , "Lexus" ];
+console.log(mobilFav);
+
+var employees = [ "Irfan", "YAzid" , "dimas" ];
+console.log(employees);
+
+var primeNumbers = [ 2 , 3 , 5 , 7 , 11 , 13 , 17 , 19 , 23 , 29 , 31 ];
+console.log(primeNumbers);
+
+var randomVariables = [ 2 , "any type works" , undefined , null , true , 2.51 ];
+console.log(randomVariables);
+
+myArray = [ "zero" , "one" , "two" ];
+console.log(myArray);
+
+window.alert ( myArray[ 0 ]);
+Elementnumber = 1;
+window.alert (myArray[Elementnumber]);
+myobject = {};
+
+john = { firstname: "John" , lastname: "Doe" , fullname: "John Doe" };
+console.log(john);
+
+billy = {
+firstname : "irfan" ,
+lastname: undefined ,
+fullname: "Ahmad Irfan"
+};
+
+window.alert ( john.fullname ); 
+window.alert ( billy.firstname ); 
+
+
+
+
+
+
+
+
